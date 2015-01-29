2f5fd2a1c1a932116cc0147f1aeeabbcfa01418c
Michael Jennings <mpjngs@gmail.com>
2015-01-29 13:29:18 -0800

    simplified image loading

--- images.js
+++ images.js
@@ -5,60 +5,25 @@ var container = document.getElementById("bwork");
 
 function imageReel(images){
 
-  var currentNum = 0;
-  var currentImage = new Image();
-  
-  currentImage.src = url + images[0];
-  imageElement.src = currentImage.src;
-  
-  var nextImage = new Image();
-  nextImage.src = url + images[1];
-  
-  var prevImage = null;
+  var loadImage = [];
+
+  loadImage[0] = new Image();
+  loadImage[0].src = url + images[0];
+  imageElement.src = url + images[0];
+
+  loadImage[1] = new Image();
+  loadImage[1].src = url + images[1];
 
   return function (num){
-    if(num - 1 === currentNum){
-      if(nextImage != null){
-        prevImage = currentImage;
-        currentImage = nextImage;
-        imageElement.src = currentImage.src;
-        currentNum++;
-        if(currentNum != images.length - 1){
-          nextImage = new Image();
-          nextImage.src = url + images[currentNum+1] ;
-        } else {
-          nextImage = null;
-        }
-      }
-  
-    } else if(num + 1 === currentNum) {
-      if(prevImage != null){
-        nextImage = currentImage;
-        currentImage = prevImage;
-        imageElement.src = currentImage.src;
-        currentNum--;
-        if(currentNum !== 0){
-          prevImage = new Image();
-          prevImage.src = url + images[currentNum-1];
-        } else {
-          prevImage = null;
-        }
-      }
-    } else if(num >= 0 && num < images.length) {
-      currentImage.src = url + images[num];
-      imageElement.src = currentImage.src;
-      currentNum = num;
-      if(currentNum != images.length - 1){
-         nextImage = new Image();
-         nextImage.src = url + images[currentNum+1] ;
-       } else {
-         nextImage = null;
+    if(num >= 0 && num < images.length) {
+      imageElement.src = url + images[num];
+      if(num != images.length - 1){
+         loadImage[0] = new Image();
+         loadImage[0].src = url + images[num+1] ;
        }
-       if(currentNum !== 0){
-         prevImage = new Image();
-         prevImage.src = url + images[currentNum-1];
-       } else {
-         prevImage = null;
+       if(num !== 0){
+         loadImage[1] = new Image();
+         loadImage[1].src = url + images[num-1];
        }
     }
   }
