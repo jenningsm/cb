dca5ee2ec786b46ecccb2590162c9957bb9a021e
Michael Jennings <mpjngs@gmail.com>
2015-02-06 19:54:52 -0800

    reorganization

---
+++ setup.js
@@ -0,0 +1,4 @@
+
+var page = setupPage(art);
+var stop = page[1];
+page[0]();
--- workpage.js
+++ workpage.js
@@ -35,39 +35,4 @@ function setupPage(page){
   return [start, stop, transition];
 }
 
-var page = setupPage(art);
-var stop = page[1];
-page[0]();
-
-var outEvent = new Event('outfinished');
-
-function switchPages(page){
-
-  var newPage = setupPage(page);
-
-  var fadeOut = stop();
-  stop = newPage[1];
-  var start = newPage[0];
-
-  var onFadeOut = function(){
-    oneTimeListener(imgElement, 'load', function(){ fade(newPage[2], true)(0) });
-    start();
-  }
-
-  oneTimeListener(document, 'outfinished', onFadeOut);
-
-  fade(fadeOut, false)(1);
-}
-
-function fade(transition, dir){
-  return (function rec(x){
-    x += (dir ? .015 : .04) * (dir ? 1 : -1);
-    transition(x);
-    if((dir && x < 1) || (!dir && x > 0)){
-      requestAnimationFrame(function() { rec(x) });
-    } else {
-      document.dispatchEvent(outEvent);
-    }
-  });
-}
 
