31a15cc2b4811aaf9e588d8ad832d6370b4d2c60
Michael Jennings <mpjngs@gmail.com>
2015-02-07 17:52:26 -0800

    small changes

--- scrollTracker.js
+++ scrollTracker.js
@@ -1,12 +1,4 @@
 
-//the path to the images
-var imagepath = "./links/";
-
-//images belonging to each page
-var art = ["cba1.jpg", "cba2.jpg", "cba3.jpg", "cba4.jpg", "cba5.jpg","cba6.jpg","cba7.jpg","cba8.jpg","cba9.jpg","cba10.jpg","cba11.jpg","cba12.jpg"];
-var photos = ["cbp1.jpg", "cbp2.jpg", "cbp3.jpg", "cbp4.jpg", "cbp5.jpg","cbp6.jpg","cbp7.jpg","cbp8.jpg","cbp9.jpg","cbp10.jpg"];
-
-
 //to simulate newtonian motion, we use the current yoffset of the page as the current time
 
 var speed = .0005;
--- setup.js
+++ setup.js
@@ -1,3 +1,11 @@
+
+//the path to the images
+var imagepath = "./links/";
+
+//images belonging to each page
+var art = ["cba1.jpg", "cba2.jpg", "cba3.jpg", "cba4.jpg", "cba5.jpg","cba6.jpg","cba7.jpg","cba8.jpg","cba9.jpg","cba10.jpg","cba11.jpg","cba12.jpg"];
+var photos = ["cbp8.jpg", "cbp2.jpg", "cbp3.jpg", "cbp4.jpg", "cbp5.jpg","cbp6.jpg","cbp7.jpg","cbp1.jpg","cbp9.jpg","cbp10.jpg"];
+
 var page = workPage(art);
 var stop = page[1];
 page[0]();
