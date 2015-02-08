63d8d0a42a54d5fc056706679cb1f1bdfbb590c1
Michael Jennings <mpjngs@gmail.com>
2015-02-07 22:14:35 -0800

    added the illustration and design workpages

--- setup.js
+++ setup.js
@@ -1,10 +1,12 @@
 
 //the path to the images
-var imagepath = "./links/";
+var imagepath = "http://www.charlibeck.com/links/";
 
 //images belonging to each page
 var art = ["cba1.jpg", "cba2.jpg", "cba3.jpg", "cba4.jpg", "cba5.jpg","cba6.jpg","cba7.jpg","cba8.jpg","cba9.jpg","cba10.jpg","cba11.jpg","cba12.jpg"];
 var photos = ["cbp8.jpg", "cbp2.jpg", "cbp3.jpg", "cbp4.jpg", "cbp5.jpg","cbp6.jpg","cbp7.jpg","cbp1.jpg","cbp9.jpg","cbp10.jpg"];
+var design = ["cbd6.jpg", "cbd2.jpg", "cbd3.jpg", "cbd4.jpg", "cbd5.jpg","cbd1.jpg","cbd7.jpg"];
+var illustration = ["cbi1.jpg", "cbi2.jpg", "cbi3.jpg", "cbi4.jpg", "cbi5.jpg","cbi6.jpg","cbi7.jpg","cbi8.jpg","cbi9.jpg","cbi10.jpg","cbi11.jpg"];
 
 var page = frontPage();
 var stop = page[1];
