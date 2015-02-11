3a7d2646f92cbf38dbb43a1a1b2d340a09d53c14
Michael Jennings <mpjngs@gmail.com>
2015-02-10 16:20:35 -0800

    fixed repeating back issue, still no link lock tho

--- pages.js
+++ pages.js
@@ -15,41 +15,40 @@ function fade(transition, dir){
 
 
 var stop = null;
-var currPage = null;
 
 //maps is defined in setup.js
 
 function switchPages(switchTo){
- if(switchTo !== currPage){
-    currPage = switchTo;
-    var newPage = maps[switchTo]();
-    var start = newPage.start;
+  var newPage = maps[switchTo]();
+  var start = newPage.start;
 
-    var onFadeOut = function(){
-      start();
-      fade(newPage.transition, true)(0);
-    }
-    oneTimeListener(document, 'fadedOut', onFadeOut);
-
-    if(stop !== null){
-      var fadeOut = stop();
-      fade(fadeOut, false)(1);
-    } else {
-      var outEvent = new Event("fadedOut");
-      document.dispatchEvent(outEvent);
-    }
-    stop = newPage.stop;
+  var onFadeOut = function(){
+    start();
+    fade(newPage.transition, true)(0);
   }
+  oneTimeListener(document, 'fadedOut', onFadeOut);
+
+  if(stop !== null){
+    var fadeOut = stop();
+    fade(fadeOut, false)(1);
+  } else {
+    var outEvent = new Event("fadedOut");
+    document.dispatchEvent(outEvent);
+  }
+  stop = newPage.stop;
 }
 
-var first = true;
+var currPage = null;
 
 function toPage(page){
-  if(first){
-    history.replaceState({'page' : page }, "", page === 'index' ? root : root + '/' + page);
-    first = false;
-  } else {
-    history.pushState({'page' : page }, "", page === 'index' ? root : root + '/' + page);
+  if(currPage !== page){
+    if(currPage === null){
+      history.replaceState({'page' : page }, "", page === 'index' ? root : root + '/' + page);
+      first = false;
+    } else {
+      history.pushState({'page' : page }, "", page === 'index' ? root : root + '/' + page);
+    }
+    currPage = page;
+    switchPages(page);
   }
-  switchPages(page);
 }
