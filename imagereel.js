f186bda58044638ba775c05594d6782ad829c0b6
Michael Jennings <mpjngs@gmail.com>
2015-02-16 14:02:18 -0800

    load all images on page open

--- imagereel.js
+++ imagereel.js
@@ -7,20 +7,13 @@ function loadImage(src){
 
 function imageReel(images){
 
-  loadImage(imagepath+images[0]);
-  loadImage(imagepath+images[1]);
-  loadImage(imagepath+images[images.length - 1]);
+  for(var i = 0; i < images.length; i++){
+    loadImage(imagepath + images[i]);
+  }
 
   return function (num){
     if(num >= 0 && num < images.length) {
       imgElement.src = imagepath + images[num];
-
-      //make sure the next and previous images are loaded so they're
-      //  ready to go when we scroll to them
-       loadImage(imagepath+images[(num+1) % images.length]);
-      if(num !== 0){
-        loadImage(imagepath+images[num-1]);
-      }
     }
   }
 }
