44735f195cdd06c78b602d53a7b76150e130638d
Michael Jennings <mpjngs@gmail.com>
2015-02-17 13:15:16 -0800

    added instructions

--- minifyjs.sh
+++ minifyjs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#remove the # from before the following two lines:
+#sudo apt-get install python-pip
+#sudo pip install jsmin
 python -m jsmin init.js utils.js imagereel.js frame.js painters.js scrollTracker.js scroller.js workpage.js frontpage.js staging.js transitions.js setup.js > my.min.js
